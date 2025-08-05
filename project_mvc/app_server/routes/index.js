01e3e4c31ed2bcdb32eb8937f473e6c182b95d25
Shoaib954 <mohdshoaib2122@gmail.com>
2025-08-05 15:07:37 +0530

    Update index.js

--- project_mvc/app_server/routes/index.js
+++ project_mvc/app_server/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const ctrlMain = require(/controllers/main);
+const ctrlMain = require('../controllers/main');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
